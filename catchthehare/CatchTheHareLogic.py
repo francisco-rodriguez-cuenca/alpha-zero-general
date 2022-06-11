b06f90cd4490f0e2c484c5db9d74d57f8cb9ae85
francisco.rodriguez.cuenca <francisco.rodriguez.cuenca@protonmail.com>
2022-06-11 18:17:16 +0200

    get initial leagl moves

--- catchthehare/CatchTheHareLogic.py
+++ catchthehare/CatchTheHareLogic.py
@@ -12,38 +12,91 @@ Based on the board for the game of Othello by Eric P. Nichols and tictacToe by E
 
 '''
 # from bkcharts.attributes import color
+from turtle import position
+import numpy as np
+
+
 class Board():
 
     # list of all 8 directions on the board, as (x,y) offsets
     __directions = [(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1)]
+    __jumps = [(2,2),(2,0),(2,-2),(0,-2),(-2,-2),(-2,0),(-2,2),(0,2)]
 
-    def __init__(self, n=3):
+    def __init__(self):
         "Set up initial board configuration."
 
-        self.n = n
+        self.n = 5
         # Create the empty board array.
-        self.pieces = [None]*self.n
-        for i in range(self.n):
-            self.pieces[i] = [0]*self.n
+
+        # 1 indicates a hunter and -1 indicates a hare
+
+        self.pieces = [
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 0, -1, 0, 1],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ]
 
     # add [][] indexer syntax to the Board
     def __getitem__(self, index): 
         return self.pieces[index]
 
+    def color_positions(self, color):
+        "Returns the position of a piece on the board as a tuple"
+
+        pos = np.argwhere(np.array(self.pieces) == color).tolist()
+
+        return pos
+
+    def legal_move(self, position):
+        "Returns if a move is legal"
+        return (
+            (position[0] >= 0) and (position[0] < 5) 
+            and (position[1] >= 0) and (position[1] < 5) 
+            and self.pieces[position[0]][position[1]] == 0
+        )
+
+    def get_moves(self, piece_pos, color):
+        """
+        Return posible next positions based on the type of piece, its position on the board, and the board itself
+        ---
+        Returns a numpy array of posible moves
+        """
+
+
+        if (piece_pos[0] + piece_pos[1]) % 2 == 0:
+            movement_set = np.array(
+                [
+                    (-1,-1), (0,-1), (1,-1),
+                    (-1, 0),         (1, 0), 
+                    (-1, 1), (0, 1), (1, 1)
+                ]
+            )
+        else:
+            movement_set = np.array(
+                [
+                            (0,-1),
+                    (-1, 0),         (1, 0), 
+                            (0, 1),
+                ]
+            )
+
+        initial_legal_moves = [(m[0]+piece_pos[0], m[1]+piece_pos[1]) for m in movement_set if self.legal_move([m[0]+piece_pos[0], m[1]+piece_pos[1]])]
+
+        return initial_legal_moves
+
     def get_legal_moves(self, color):
         """Returns all the legal moves for the given color.
-        (1 for white, -1 for black)
+        (1 for hunters, -1 for hare)
         @param color not used and came from previous version.        
         """
-        moves = set()  # stores the legal moves.
 
-        # Get all the empty squares (color==0)
-        for y in range(self.n):
-            for x in range(self.n):
-                if self[x][y]==0:
-                    newmove = (x,y)
-                    moves.add(newmove)
-        return list(moves)
+        pieces_pos = self.color_positions(color)
+
+        moves = [(tuple(p), self.get_moves(p, color)) for p in pieces_pos]
+
+        return moves
 
     def has_legal_moves(self):
         for y in range(self.n):
@@ -100,3 +153,9 @@ class Board():
         assert self[x][y] == 0
         self[x][y] = color
 
+if __name__ == "__main__":
+    
+    b = Board()
+
+    print(b.get_legal_moves(-1))
+    print(b.get_legal_moves(1))
