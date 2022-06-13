d9377a8316ff2e03762bfebbad576079499d8dbb
francisco.rodriguez.cuenca <francisco.rodriguez.cuenca@protonmail.com>
2022-06-13 23:22:47 +0200

    less mtc

--- pit.py
+++ pit.py
@@ -33,7 +33,7 @@ hp = HumanAlquerquePlayer(g).play
 n1 = NNet(g)
 n1.load_checkpoint('./temp','temp.pth.tar')
 
-args1 = dotdict({'numMCTSSims': 50, 'cpuct':12.0})
+args1 = dotdict({'numMCTSSims': 25, 'cpuct':12.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
