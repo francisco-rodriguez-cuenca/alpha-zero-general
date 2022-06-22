f137c3449907d98d126f481a5ca4e26a282cc098
francisco.rodriguez.cuenca <francisco.rodriguez.cuenca@protonmail.com>
2022-06-23 00:31:44 +0200

    chained executions

---
+++ main_middle.py
@@ -0,0 +1,64 @@
+import logging
+import sys
+
+import coloredlogs
+
+from Coach import Coach
+from alquerque.AlquerqueGame import AlquerqueGame
+from alquerque.keras.NNet import NNetWrapper as nn
+from utils import *
+
+import tensorflow.compat.v2 as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+sys.setrecursionlimit(199999)
+
+log = logging.getLogger(__name__)
+
+coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
+
+args = dotdict({
+    'numIters': 5,
+    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
+    'tempThreshold': 15,        #
+    'updateThreshold': 0.55,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+    'maxlenOfQueue': 2000,    # Number of game examples to train the neural networks.
+    'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
+    'arenaCompare': 20,         # Number of games to play during arena play to determine if new net will be accepted.
+    'cpuct': 12,
+
+    'checkpoint': './temp/',
+    'load_model': True,
+    'load_folder_file': ('./temp','best.pth.tar'),
+    'numItersForTrainExamplesHistory': 12,
+
+})
+
+
+def main():
+    log.info('Loading %s...', AlquerqueGame.__name__)
+    g = AlquerqueGame()
+
+    log.info('Loading %s...', nn.__name__)
+    nnet = nn(g)
+
+    if args.load_model:
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file[0], args.load_folder_file[1])
+        nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+    else:
+        log.warning('Not loading a checkpoint!')
+
+    log.info('Loading the Coach...')
+    c = Coach(g, nnet, args)
+
+    if args.load_model:
+        log.info("Loading 'trainExamples' from file...")
+        c.loadTrainExamples()
+
+    log.info('Starting the learning process 🎉')
+    c.learn()
+
+
+if __name__ == "__main__":
+    main()
