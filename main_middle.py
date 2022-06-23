4edd65bbe1e98082b01ed6068af5e49254a4b3fb
francisco.rodriguez.cuenca <francisco.rodriguez.cuenca@protonmail.com>
2022-06-23 09:55:18 +0200

    100_80b3_20 ep

--- main_middle.py
+++ main_middle.py
@@ -23,7 +23,7 @@ args = dotdict({
     'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.55,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
-    'maxlenOfQueue': 100000,    # Number of game examples to train the neural networks.
+    'maxlenOfQueue': 50000,    # Number of game examples to train the neural networks.
     'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
     'arenaCompare': 20,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 12,
