9102a19629530acfd312258004e946a9e4f9e2cf
UPCONT\frodriguezc <frodriguezc@comillas.edu>
2022-06-27 16:26:12 +0200

    necessary changes

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from alquerque.keras.NNet import NNetWrapper as nn
 from utils import *
 
 import tensorflow.compat.v2 as tf
-physical_devices = tf.config.list_physical_devices('GPU') 
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 sys.setrecursionlimit(199999)
 
