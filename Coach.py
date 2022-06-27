76edc136a330a400b45249deac1690f3bc837ab4
francisco.rodriguez.cuenca <francisco.rodriguez.cuenca@protonmail.com>
2022-06-27 21:59:16 +0200

    Optimize pickle

--- Coach.py
+++ Coach.py
@@ -3,6 +3,7 @@ import os
 import sys
 from collections import deque
 from pickle import Pickler, Unpickler
+import pickle, pickletools
 from random import shuffle
 
 import numpy as np
@@ -136,7 +137,9 @@ class Coach():
             os.makedirs(folder)
         filename = os.path.join(folder, self.getCheckpointFile(iteration) + ".examples")
         with open(filename, "wb+") as f:
-            Pickler(f).dump(self.trainExamplesHistory)
+            pickled = pickle.dumps(self.trainExamplesHistory)
+            optimized_pickle = pickletools.optimize(pickled)
+            f.write(optimized_pickle)
         f.closed
 
     def loadTrainExamples(self):
