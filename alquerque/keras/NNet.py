e191ecffc3c9d5aa170f41dac55089430726b7d7
UPCONT\frodriguezc <frodriguezc@comillas.edu>
2022-06-27 16:52:22 +0200

    return to h5

--- alquerque/keras/NNet.py
+++ alquerque/keras/NNet.py
@@ -70,7 +70,7 @@ class NNetWrapper(NeuralNet):
             os.mkdir(folder)
         else:
             print("Checkpoint Directory exists! ")
-        self.nnet.model.save_weights(filepath)
+        self.nnet.model.save_weights(filepath, save_format = 'h5')
         print(filepath)
 
     def load_checkpoint(self, folder='checkpoint', filename='checkpoint.pth.tar'):
