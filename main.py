ca4821406e079a3b6c4041752e19cb526d2f82e9
UPCONT\frodriguezc <frodriguezc@comillas.edu>
2022-06-27 16:20:33 +0200

    Memory growth cannot differ between GPU devices

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from utils import *
 
 import tensorflow.compat.v2 as tf
 physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 sys.setrecursionlimit(199999)
 
